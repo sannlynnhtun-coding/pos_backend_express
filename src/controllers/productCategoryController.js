e6167e13acaf90d86318124c92f8d630efec0350
Swan Yee Tun <swanyeetun.2000@gmail.com>
2024-04-15 02:17:52 +0630

    project setup

---
+++ src/controllers/productCategoryController.js
@@ -0,0 +1,11 @@
+class productCategoryController {
+  list(request, response) {
+    return response.status(200).send(data);
+  }
+  get() {}
+  create() {}
+  update() {}
+  delete() {}
+}
+
+export default productCategoryController;
--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,23 @@
-import express  from "express";
-import protductCategoriesRouter from '../src/routes/productCategories.mjs'
-
-
+import express from "express";
+import protductCategoriesRouter from "../src/routes/productCategories.mjs";
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
 app.use(express.json());
-app.use(protductCategoriesRouter)
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+
+// Routers
+app.use(protductCategoriesRouter);
+
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3500;
-
-
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
